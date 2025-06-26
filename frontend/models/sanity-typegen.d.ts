8fd5fa1309cc99057bcc815638de3357e22ca313
Rafa Canosa <rafacv23@gmail.com>
2025-06-27 01:26:14 +0200

    Refactor code structure for improved readability and maintainability

---
+++ frontend/models/sanity-typegen.d.ts
@@ -0,0 +1,5 @@
+{
+  ;("schemaPath")
+  "./models/sanity-schema.json", "outputPath"
+  ;("./models/sanity-types.ts")
+}
---
+++ frontend/sanity.config.ts
@@ -0,0 +1,11 @@
+﻿import { defineConfig } from "sanity";
+
+export default defineConfig({
+  schema: {
+    types: [],
+    typegen: {
+      sourceFile: "./models/sanity-schema.json",
+      outputFile: "./models/sanity-types.ts"
+    }
+  }
+});
