eb234116508032946492552bf0c36564a3f87438
Rafa Canosa <rafacv23@gmail.com>
2025-06-27 01:28:21 +0200

    Add projectId and dataset to Sanity configuration

--- frontend/sanity.config.ts
+++ frontend/sanity.config.ts
@@ -1,11 +1,14 @@
 ﻿import { defineConfig } from "sanity";
 
 export default defineConfig({
+  projectId: "your-project-id",
+  dataset: "production",
   schema: {
     types: [],
     typegen: {
       sourceFile: "./models/sanity-schema.json",
       outputFile: "./models/sanity-types.ts"
     }
-  }
+  },
+  plugins: []
 });
